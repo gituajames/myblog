f35e72128dfda5af27e16f8b1e93b1caa56877b9
gituajames <gitua85@gmail.com>
2017-07-30 19:07:24 +0300

    templates

--- mysite/blog/url.py
+++ mysite/blog/url.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^(?P<name>pattern)', )  # named groups url pattern
+    # url(r'^(?P<name>pattern)', )  # named groups url pattern
 ]
